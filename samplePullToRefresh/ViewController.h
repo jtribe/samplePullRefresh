7eae826f248a52cb296ba2ea3c2828df5c4a2158
Gerald Kim <gerald@jtribe.com.au>
2013-08-14 17:00:37 +1000

    Initial Commit

---
+++ samplePullToRefresh/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  samplePullToRefresh
+//
+//  Created by Gerald Kim on 14/08/13.
+//  Copyright (c) 2013 jtribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ samplePullToRefresh/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  samplePullToRefresh
+//
+//  Created by Gerald Kim on 14/08/13.
+//  Copyright (c) 2013 jtribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
